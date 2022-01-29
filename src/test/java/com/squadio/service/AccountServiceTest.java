398dd0ad3312381c5f8dc49304151e9e4f3a9d0e
Bashar-Othman <bashar.othman.message@gmail.com>
2022-01-29 16:54:29 +0200

    Update AccountServiceTest.java

--- src/test/java/com/squadio/service/AccountServiceTest.java
+++ src/test/java/com/squadio/service/AccountServiceTest.java
@@ -48,8 +48,6 @@ public class AccountServiceTest {
         List<Account> expResult = null;
         List<Account> result = instance.readAll();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
