24d2f5309ecf249dbfb8810278e4ecdbe2678e33
Bashar-Othman <bashar.othman.message@gmail.com>
2022-01-29 16:54:57 +0200

    Update UserServiceTest.java

--- src/test/java/com/squadio/service/UserServiceTest.java
+++ src/test/java/com/squadio/service/UserServiceTest.java
@@ -49,8 +49,6 @@ public class UserServiceTest {
         List<User> expResult = null;
         List<User> result = instance.readAll();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
